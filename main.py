3beb44f4ae491eaccb82d3c4eec231c4bfc781c5
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-01-07 13:26:11 -0800

    Adjust train-test split size

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ def learn_data():
 
     # This is the neural network
     classifier = MLPClassifier(random_state=0)
-    print()
-    test_size = 10
+    test_size = 240
+
+    train_inputs = inputs[test_size:]
+    train_outputs = outputs[test_size:]
+    test_inputs = inputs[:test_size]
+    test_outputs = outputs[:test_size]
 
     # Train on all the data AFTER the first 10 (i.e. on 1787 images)
-    classifier.fit(inputs[test_size:], outputs[test_size:])
+    classifier.fit(train_inputs, train_outputs)
 
     # Test on ONLY the first 10 digits
     # (which coincidentally are themselves the digits 1,2,3,4,5,6,7,8,9 in order)
-    results = classifier.predict(inputs[:test_size])
+    results = classifier.predict(test_inputs)
 
-    print(f'Accuracy: {(results == outputs[:test_size]).mean()}')
+    print(f'Accuracy: {(results == test_outputs).mean()}')
 
 if __name__ == '__main__':
     learn_data()
