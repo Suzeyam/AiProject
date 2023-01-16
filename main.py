af4ca1d5ac230e277516123f6c2ef54eaa613755
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-01-16 15:37:13 -0800

    Plot losses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.utils import shuffle
 import numpy as np
+import matplotlib.pyplot as plt
 
 def learn_data():
     inputs = np.genfromtxt(
@@ -10,10 +11,7 @@ def learn_data():
         'life_expectancy_data.csv', dtype=str, delimiter=',', skip_header=1, usecols=2
     ) == 'Developed'
 
-    # This is the neural network
-    classifier = MLPClassifier(random_state=0)
     test_size = 240
-
     inputs, outputs = shuffle(inputs, outputs, random_state=10)
 
     train_inputs = inputs[test_size:]
@@ -21,17 +19,42 @@ def learn_data():
     test_inputs = inputs[:test_size]
     test_outputs = outputs[:test_size]
 
-    # Train on all the data AFTER the first 10 (i.e. on 1787 images)
-    classifier.fit(train_inputs, train_outputs)
+    best_losses = []
+    hidden_layer_sizes = [
+        (100,), (100, 100), (100, 100, 100), (100, 100, 100, 100), (100, 100, 100, 100, 100)
+    ]
+    for i, hls in enumerate(hidden_layer_sizes):
 
-    # Test on ONLY the first 10 digits
-    # (which coincidentally are themselves the digits 1,2,3,4,5,6,7,8,9 in order)
-    test_results = classifier.predict(test_inputs)
-    train_results = classifier.predict(train_inputs)
+        classifier = MLPClassifier(random_state=0, hidden_layer_sizes=hls)
 
-    print(f'Test Accuracy: {(test_results == test_outputs).mean() * 100:.5f}')
+        # Train on all the data AFTER the first 10 (i.e. on 1787 images)
+        classifier.fit(train_inputs, train_outputs)
 
-    print(f'Train Accuracy: {(train_results == train_outputs).mean() * 100:.5f}')
+        # Test on ONLY the first 10 digits
+        # (which coincidentally are themselves the digits 1,2,3,4,5,6,7,8,9 in order)
+        test_results = classifier.predict(test_inputs)
+        train_results = classifier.predict(train_inputs)
+
+        print(f'\nCurrent iteration: {i}')
+        print(f'Hidden Layer used: {hls}')
+        print(f'Test Accuracy: {(test_results == test_outputs).mean() * 100:.5f}')
+        print(f'Train Accuracy: {(train_results == train_outputs).mean() * 100:.5f}')
+        print(f'Best loss: {classifier.best_loss_}')
+        best_losses.append(classifier.best_loss_)
+
+        '''
+        plt.title(f'Neural Net Loss with hidden layers: {hls}')
+        plt.plot(range(len(classifier.loss_curve_)), classifier.loss_curve_)
+        plt.xlabel('Iteration Number')
+        plt.ylabel('Loss')
+        plt.show()
+        '''
+
+    plt.title('Best Loss for each number of layers')
+    plt.plot([len(e) for e in hidden_layer_sizes], best_losses)
+    plt.xlabel('Number of layers trained')
+    plt.ylabel('Best Loss Attained')
+    plt.show()
 
 if __name__ == '__main__':
     learn_data()
