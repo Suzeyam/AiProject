b09cd0105c09d5fdc6c20a6d911850ff56db631d
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-01-28 13:01:48 -0800

    Attempt validation loss

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.utils import shuffle
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
+
 def learn_data():
     inputs = np.genfromtxt(
         'life_expectancy_data.csv', dtype=np.float32, delimiter=',', skip_header=1, usecols=range(3, 22)
@@ -19,10 +19,9 @@ def learn_data():
     test_inputs = inputs[:test_size]
     test_outputs = outputs[:test_size]
 
-    best_losses = []
-    hidden_layer_sizes = [
-        (100,), (100, 100), (100, 100, 100), (100, 100, 100, 100), (100, 100, 100, 100, 100), (100,100,100,100), (100,100,100), (100,100), (100)
-    ]
+    best_train_losses = []
+    best_validation_losses = []
+    hidden_layer_sizes = [(100,) * i for i in range(1, 20)]
     for i, hls in enumerate(hidden_layer_sizes):
 
         classifier = MLPClassifier(random_state=0, hidden_layer_sizes=hls)
@@ -40,7 +39,8 @@ def learn_data():
         print(f'Test Accuracy: {(test_results == test_outputs).mean() * 100:.5f}')
         print(f'Train Accuracy: {(train_results == train_outputs).mean() * 100:.5f}')
         print(f'Best loss: {classifier.best_loss_}')
-        best_losses.append(classifier.best_loss_)
+        best_train_losses.append(classifier.best_loss_)
+        best_validation_losses.append(classifier.validation_scores_.max())
 
         '''
         plt.title(f'Neural Net Loss with hidden layers: {hls}')
@@ -50,8 +50,17 @@ def learn_data():
         plt.show()
         '''
 
-    plt.title('Best Loss for each number of layers')
-    plt.plot([len(e) for e in hidden_layer_sizes], best_losses)
+    plt.title('Best Losses for each number of layers')
+    plt.plot(
+        [len(e) for e in hidden_layer_sizes],
+        best_train_losses,
+        label='Train loss'
+    )
+    plt.plot(
+        [len(e) for e in hidden_layer_sizes],
+        best_validation_losses,
+        label='Validation loss'
+    )
     plt.xlabel('Number of layers trained')
     plt.ylabel('Best Loss Attained')
     plt.show()
